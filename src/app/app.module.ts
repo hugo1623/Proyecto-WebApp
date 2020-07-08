7f32674c09fb58d54d4af54b989811d1d77b352a
Hugo Luis <hugoluis.system@gmail.com>
2020-07-08 03:48:26 -0500

    Sección de Productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { ComponentsComponent } from './components/components.component';
 //Rutas
 import{routing,appRoutinProviders} from './app.routing';
 import { ErrorComponent } from './components/error/error.component';
+import { ProductoComponent } from './components/producto/producto.component';
 @NgModule({
   declarations: [
     AppComponent,
     ComponentsComponent,
-    ErrorComponent
+    ErrorComponent,
+    ProductoComponent
     
   ],
   imports: [
