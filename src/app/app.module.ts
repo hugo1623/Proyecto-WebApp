88689587fe6fd9cb94cfa071aa75be1a68e46ca7
Hugo Luis <hugoluis.system@gmail.com>
2020-07-13 13:53:43 -0500

    Creación de formulario para añadir productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -25,6 +26,7 @@ import { ProductoAddComponent } from './components/producto-add/producto-add.com
   imports: [
     BrowserModule,
     routing,
+    FormsModule,
     HttpClientModule,
     RouterModule
   ],
